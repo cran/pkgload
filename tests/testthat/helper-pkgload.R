716902165d33e52407f86a822a7885a754c5b02d
Lionel Henry <lionel@posit.co>
2024-01-16 12:20:04 +0000

    version 1.3.4

--- tests/testthat/helper-pkgload.R
+++ tests/testthat/helper-pkgload.R
@@ -19,10 +19,15 @@ suppress_output <- function(expr) {
   )
 }
 
-# Need to also specify `LC_ALL` because `LANGUAGE` is ignored when
-# `LANG` is set (here via `LC_ALL`) to `C` or `C.UTF-8`
-with_lang <- function(lc, language, expr) {
-  withr::local_envvar(c(LC_ALL = lc))
-  withr::local_language(language)
-  expr
+# Used to skip tests that will fail when locale is set to C, for
+# instance `with_language()` tests. These tests should only be run
+# when using a locale like `en_US`.
+skip_if_c_locale <- function() {
+  lc_all <- Sys.getenv("LC_ALL", "")
+  skip_if(lc_all %in% c("C", "C.UTF-8"))
+
+  if (lc_all == "") {
+    lang <- Sys.getenv("LANG", "")
+    skip_if(lang %in% c("C", "C.UTF-8"))
+  }
 }
